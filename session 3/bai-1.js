81f20b8ab72cd90940cf02296ef3977a30311893
nhviet90 <52965513+nhviet90@users.noreply.github.com>
2019-07-25 23:07:20 +0700

    Home work session 3

--- session 3/bai-1.js
+++ session 3/bai-1.js
@@ -1,1 +1,3 @@
-let ten = promt
+let ten_tuoi = promt("Hay cho biet ten, tuoi cua ban");
+let vi_tri_dau_phay = ten_tuoi.indexOf(",");
+console.log(`Ten ban la ${ten_tuoi}.substr(0,${vi_tri_dau_phay}-1)`);
