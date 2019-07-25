1cf80ee3a5ca168e14d0fe3b7a738ce472c2c138
Viet <soul20290@gmail.com>
2019-07-25 19:02:39 +0700

    Sec 2 + 3

---
+++ session 2/my-java.js
@@ -0,0 +1,44 @@
+// // var x = 5;
+// // let Y="Xin chào";
+// // const pi = 3.14;
+// // let xinchao = "xin chào";
+// // let Xinchao = "Xin chào 2"
+// // var welcome = "J4E"
+// // var z = 10;
+// // var i = x / z ;
+// // var a = x - z;
+
+
+// // console.log("Gia tri cua a la " + a);
+// // console.log(i);
+// // console.log(welcome);
+// // console.log(pi);
+// // console.log(xinchao);
+// // console.log(Xinchao);
+// // console.log(x);
+// // console.log(Y);
+
+// // pi = 5; Khai bao loi  hang so
+
+// let x = 10;
+// {   
+//     let x = 20;
+//     console.log(x);
+    
+    
+// }
+// console.log(x);
+
+// x = 10;
+// console.log(x);
+// var x;
+
+var x;
+let y;
+console.log(x);
+console.log(y);
+x = 20;
+y = 10;
+
+console.log(x);
+console.log(y);
---
+++ session 3/datatypes.js
@@ -0,0 +1,38 @@
+// let x = 100;
+// let y = 20
+
+// // console.log(x);
+// // console.log(y);
+
+// // console.log("Tổng của "+ x + " và " + y +" là: " + (x + y));
+// // console.log(`Tổng của ${x} và ${y} là: ${x+y}`);
+
+
+// var x = 100;
+// console.log(x++);
+// console.log(x);
+
+// var x = 100;
+// console.log(++x);
+// console.log(x);
+
+// var x = 100;
+// console.log(x--);
+// var x = 100;
+// console.log(--x);
+
+// let yearOfAge = prompt("Ban bao nhieu tuoi")
+// console.log(yearOfAge);
+
+let yearOfBirth = prompt("Ban sinh nam bao nhieu")
+var yearOfAge = 2019 - yearOfBirth;
+console.log(yearOfAge);
+
+let name = prompt("Ten ban la gi");
+console.log(`Chao ban ${name}, ban ${yearOfAge}`);
+
+
+
+
+
+
