8ed89451f14b233b1b89c082c9116987f933acee
Viet <soul20290@gmail.com>
2019-07-29 19:38:23 +0700

    session 4

---
+++ session 4/loop.js
@@ -0,0 +1,51 @@
+
+// let nhap = Number(prompt("Nhap so can tinh tu 1 den N"));
+// let tong = 0
+// for (let index = 0; index < nhap + 1; index++) {
+//     tong = tong + index;
+    
+
+    
+// }
+// console.log(tong);
+
+
+// let so_canh = Number(prompt("Nhap vao so canh cua da giac"));
+// let chu_vi = 0;
+// for (let index = 0; index < so_canh; index++) {
+//     let chieu_dai = Number(prompt(`Chieu dai cua canh so ${index + 1} la:`));
+//     chu_vi += chieu_dai;
+// }
+// console.log(`Chu vi cua da giac la ${chu_vi}`);
+
+
+// let nhap = Number(prompt("Nhap so can tinh"));
+// let tong = 0;
+// for (let index = 1; index < nhap + 1; index++) {
+//     tong = tong + 1 / index;
+    
+// } 
+// console.log(tong);
+
+
+// let nhap = Number(prompt("Nhap so can tinh"));
+// let tong = 0;
+// for (let index = 1; index < nhap + 1; index++) {
+//     tong = tong + 1 / (index * (index + 1));
+    
+// } 
+// console.log(tong);
+
+// let index = 0;
+// while (index < 3) {
+//     console.log(index++);
+    
+// }
+
+let pw = prompt("Moi nhap mat khau : ");
+while (pw.length < 8) {
+    pw = prompt("Mat khau chua phai nhieu hon 8 ky tu, vui long nhap lai : ");
+}
+console.log(`Mat khau la ${pw}`);
+
+
