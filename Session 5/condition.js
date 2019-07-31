6bbfe4894a00f7010f3d7329c716329d9f815b4b
nhviet90 <52965513+nhviet90@users.noreply.github.com>
2019-07-31 23:17:51 +0700

    homework session 5

--- Session 5/condition.js
+++ Session 5/condition.js
@@ -46,20 +46,55 @@
     
 // }
 // Nhap a, b , tim nghiem x cho phuong trinh 
-let a = Number(prompt("Nhap a: "));
-let b = Number(prompt("Nhap b: "));
-let x;
-if (a == 0 ) {
-    if (b == 0) {
-        console.log("Phuong trinh co vo so nghiem");
+// let a = Number(prompt("Nhap a: "));
+// let b = Number(prompt("Nhap b: "));
+// let x;
+// if (a == 0 ) {
+//     if (b == 0) {
+//         console.log("Phuong trinh co vo so nghiem");
         
+//     }
+//     else {
+//         console.log("Phuong trinh vo nghiem");
+        
+//     }
+//     else {
+//         x = -b/a
+//         console.log(x);
+        
+//     }
+// Homework
+let a = Number(prompt("Nhap vao gia tri a: "));
+let b = Number(prompt("Nhap vao gia tri b: "));
+let c = Number(prompt("Nhap vao gia tri c: "));
+
+if (a == 0) {
+    if (b==0) {
+        if (c==0) {
+            console.log("Phuong trinh vo so nghiem");
+            
+        }
+        else{
+            console.log("Phuong trinh vo nghiem");
+            
+        }
     }
-    else {
+     else {
+         console.log("nghiem x cua phuong trinh la ") + -c/b;
+         
+     }
+}
+else{
+    let delta = b*b - 4*a*c;
+    if (delta < 0) {
         console.log("Phuong trinh vo nghiem");
         
     }
+    else if (delta == 0) {
+        console.log("Phuong trinh co nghiem x bang: " + -b/(2*a));
+    }
     else {
-        x = -b/a
-        console.log(x);
-        
+        console.log("Phuong trinh co 2 nghiem la " + ((-b + Math.sqrt(delta))/(2*a)) + " va " + ((-b - Math.sqrt(delta))/(2*a)));
+
+    }
     }
