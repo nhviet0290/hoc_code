7170a0e6a7c257a55a69bf5fdeb865598f0cbea3
nhviet90 <soul20290@gmail.com>
2019-08-01 18:15:25 +0700

    bai tap thuc hanh

---
+++ bai-tap/bai-tap.js
@@ -0,0 +1,19 @@
+//B1 Nhập vào bán kính hình tròn, in ra chu vi và diện tích hình tròn.
+// let bankinh = Number(prompt("Nhap vao ban kinh hinh tron: "));
+// let pi = 3.14;
+// let chuvi = 2 * bankinh * pi;
+// let dientich = bankinh * bankinh * pi;
+// console.log("Chu vi hinh tron la: " + chuvi);
+// console.log("Dien tich hinh tron la: " + dientich);
+
+//B2 Nhập vào chiều dài và chiều rộng, in ra chu vi và diện tích hình chữ nhật
+// let dai = Number(prompt("Nhap vao chieu dai: "));
+// let rong = Number(prompt("Nhap vao chieu rong: "));
+// let chuvi = 2 * (dai + rong);
+// let dientich = dai * rong;
+// console.log("Chu vi la: " + chuvi);
+// console.log("Dien tich la: " + dientich);
+
+//B3
+let x = Number(prompt("Nhap gia tri của x: "));
+let y = 4 * ();
