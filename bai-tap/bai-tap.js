86e9a32f22a6eb6010f30136dc7500c5a132f7fe
nhviet90 <soul20290@gmail.com>
2019-08-05 13:01:05 +0700

    lam bai 4

--- bai-tap/bai-tap.js
+++ bai-tap/bai-tap.js
@@ -14,6 +14,35 @@
 // console.log("Chu vi la: " + chuvi);
 // console.log("Dien tich la: " + dientich);
 
-//B3
-let x = Number(prompt("Nhap gia tri của x: "));
-let y = 4 * ();
+// //B3 
+// let x = Number(prompt("Nhap gia tri của x: "));
+// let y1 = 4 * (x*x + 10 * x * Math.sqrt(x) + 3 * x + 1 );
+// let y2 = (Math.sin(Math.PI * x * x) + Math.sqrt(x*x + 1)) / (Math.exp(2*x) + Math.cos((Math.PI * x) / x))
+
+// console.log(y1);
+// console.log(y2);
+
+//B4
+let sotien = Number(prompt("Nhap so tien can gui: "));
+let to100 = Math.floor(sotien/ 100000);
+let du100 = sotien - to100 * 100000 ;
+let to50 = Math.floor(du100/ 50000);
+let du50 = du100 - to50 * 50000;
+let to20 = Math.floor(du50/ 20000);
+let du20 = du50 - to20 * 20000 ;
+let to10 = Math.floor(du20 / 10000);
+if (du100 > 0 ) {
+    if (du50 > 0) {
+        if (du20 > 0) {
+            console.log("So tien doi thanh :" + to100 + " tờ 100k " + to50 + " tờ 50k " + to20 + " tờ 20k " + to10 + " tờ 10k ");
+            
+        } else { 
+            console.log("So tien doi thanh :" + to100 + " tờ 100k " + to50 + " tờ 50k " + to20 + " tờ 20k ");
+            
+        }
+    } else {
+        console.log("So tien doi thanh :" + to100 + " tờ 100k " + to50 + " tờ 50k " );
+    }
+} else {
+    console.log("So tien doi thanh :" + to100 + " tờ 100k ");
+}
