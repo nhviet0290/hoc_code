80ff5fee9c03654676e0e7955d11bb13cee91335
nhviet90 <soul20290@gmail.com>
2019-08-28 22:58:24 +0700

    to-do-list.js

--- session 9/to-do-list.js
+++ session 9/to-do-list.js
@@ -1,9 +1,11 @@
+let no = 0;
 function keyPress(event) {
     if (event.keyCode == 13) {
         let stt = document.createElement("li");
         document.getElementById("STT").appendChild(stt);
-        let content = document.createElement("tr");
+        let content = document.createElement("span");
         content.innerHTML = document.getElementById("addList").value;
         document.getElementById("content").appendChild(content);
-    }
+
+}
 }
