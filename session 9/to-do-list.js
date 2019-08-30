e6f6c18a26347d90229c760972028a59f4d18c53
nhviet90 <soul20290@gmail.com>
2019-08-30 17:39:29 +0700

    to-do-list.js

--- session 9/to-do-list.js
+++ session 9/to-do-list.js
@@ -1,17 +1,14 @@
 let no = 0;
 function keyPress(event) {
-    
-    // if (event.keyCode == 13) {
-    //     let table = document.getElementById("data");
-    //     let row = table.insertRow(-1);
-    //     let stt = row.insertCell(0);
-    //     let ol = document.createElement("ol");
-    //     let li = document.createElement("li");
-    //     ol.appendChild(li);
-    //     if (stt.innerHTML.indexOf(ol) >= 0){
-    //         stt.appendChild(li);
-    //     } else{
-    //         stt.appendChild(ol);
-    //     }
-    //     let content = row.insertCell();
-    //     content.innerHTML = document.getElementById("addList").value;
+    if (event.keyCode == 13) {
+        let table = document.getElementById("data");
+        let row = table.insertRow(-1);
+        let stt = row.insertCell(0);
+        let ol = document.createElement("ol");
+        let li = document.createElement("li");
+        ol.appendChild(li);
+        stt.appendChild(ol);
+        let content = row.insertCell();
+        content.innerHTML = document.getElementById("addList").value;
+}
+}
