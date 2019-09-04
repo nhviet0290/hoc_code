49b8238414386d5c78deb3396f8d881f1c7a7d37
nhviet90 <soul20290@gmail.com>
2019-09-04 22:56:56 +0700

    to-do-list.js

--- session 9/to-do-list.js
+++ session 9/to-do-list.js
@@ -1,14 +1,12 @@
 let toDoList = [];
-let no = 0;
+let countSTT = 0;
 function keyPress(event) {
     if (event.keyCode == 13) {
-        // dataToArray();
-        no += 1
-        // let count = toDoList.length;
+        countSTT += 1
         let table = document.getElementById("data");
         let row = table.insertRow(-1);
         let stt = row.insertCell(0);
-        stt.innerHTML = no;
+        stt.innerHTML = countSTT;
         let content = row.insertCell(-1);
         content.innerHTML = document.getElementById("addList").value;
         let btn = row.insertCell(-1);
@@ -25,24 +23,12 @@ function keyPress(event) {
         buttonDel.addEventListener('click', function delData() {
             let i = Number(buttonDel.parentNode.parentNode.childNodes[0].childNodes[0].data);
             buttonDel.parentNode.parentNode.remove();
-            for (let index = i; index <= no; index++) {
+            for (let index = i; index < countSTT; index++) {
                 document.getElementsByTagName("tr")[index].children[0].childNodes[0].data -=1;
             }
-            no -= 1; 
-            console.log(no);
-        } 
-        ) 
+            countSTT -= 1; 
+            
+        }) 
         deleteData.appendChild(buttonDel);
        }
-       
-       
     }
-
-function dataToArray() {
-    toDoList.push(document.getElementById("addList").value);
-}
-
-
-function deleteData() {
-
-}
